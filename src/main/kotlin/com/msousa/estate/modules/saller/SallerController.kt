362a5b07c964e0eb02f9d48a20d0c05523f7047b
MSousa <michael.sp.contato@gmail.com>
2022-10-15 20:31:34 -0300

    Implementado save.

--- src/main/kotlin/com/msousa/estate/modules/saller/SallerController.kt
+++ src/main/kotlin/com/msousa/estate/modules/saller/SallerController.kt
@@ -1,20 +1,22 @@
 package com.msousa.estate.modules.saller
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
-import java.util.UUID
+import java.util.*
 
 @RestController
 @RequestMapping("/sellers")
-class SallerController(val sallerRepository: SallerRepository) {
+class SallerController(val sellerRepository: SellerRepository) {
 
     @GetMapping
-    fun findAll(): Flux<Seller> = sallerRepository.findAll()
+    fun findAll(): Flux<Seller> = sellerRepository.findAll()
 
     @GetMapping("/{id}")
-    fun findById(@PathVariable id: UUID): Mono<Seller> = sallerRepository.findById(id)
+    fun findById(@PathVariable id: UUID): Mono<Seller> = sellerRepository.findById(id)
+
+    @Transactional
+    @PostMapping
+    fun create(@RequestBody saller: Seller): Mono<Seller> = sellerRepository.save(saller)
 }
--- src/main/kotlin/com/msousa/estate/modules/saller/SellerRepository.kt
+++ src/main/kotlin/com/msousa/estate/modules/saller/SellerRepository.kt
@@ -3,5 +3,5 @@ package com.msousa.estate.modules.saller
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import java.util.UUID
 
-interface SallerRepository: ReactiveCrudRepository<Seller, UUID> {
+interface SellerRepository: ReactiveCrudRepository<Seller, UUID> {
 }
