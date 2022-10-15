f59ee41ca718b0bfc6e4bae045863f4af92efdd5
MSousa <michael.sp.contato@gmail.com>
2022-10-15 17:31:05 -0300

    Implementado o findById.

--- src/main/kotlin/com/msousa/estate/modules/saller/SallerController.kt
+++ src/main/kotlin/com/msousa/estate/modules/saller/SallerController.kt
@@ -1,14 +1,20 @@
 package com.msousa.estate.modules.saller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import java.util.UUID
 
 @RestController
 @RequestMapping("/sellers")
 class SallerController(val sallerRepository: SallerRepository) {
 
     @GetMapping
-    fun list(): Flux<Seller> = sallerRepository.findAll()
+    fun findAll(): Flux<Seller> = sallerRepository.findAll()
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: UUID): Mono<Seller> = sallerRepository.findById(id)
 }
